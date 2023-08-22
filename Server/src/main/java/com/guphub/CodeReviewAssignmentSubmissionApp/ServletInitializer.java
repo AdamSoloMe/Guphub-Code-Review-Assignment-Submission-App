41c7f4a270f6c909f599028d6214af711c5fa3c8
Adam Solomon <100500109+AdamSoloMe@users.noreply.github.com>
2023-08-22 13:19:43 -0400

    changed folder Strucutre

--- Server/src/main/java/com/guphub/CodeReviewAssignmentSubmissionApp/ServletInitializer.java
+++ Server/src/main/java/com/guphub/CodeReviewAssignmentSubmissionApp/ServletInitializer.java
@@ -7,7 +7,7 @@ public class ServletInitializer extends SpringBootServletInitializer {
 
 	@Override
 	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-		return application.sources(CodeReviewAssignmentSubmissionAppApplication.class);
+		return application.sources(CodeReviewAssignmentSubmissionApplication.class);
 	}
 
 }
--- Server/src/test/java/com/guphub/CodeReviewAssignmentSubmissionApp/CodeReviewAssignmentSubmissionApplicationTests.java
+++ Server/src/test/java/com/guphub/CodeReviewAssignmentSubmissionApp/CodeReviewAssignmentSubmissionApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class CodeReviewAssignmentSubmissionAppApplicationTests {
+class CodeReviewAssignmentSubmissionApplicationTests {
 
 	@Test
 	void contextLoads() {
