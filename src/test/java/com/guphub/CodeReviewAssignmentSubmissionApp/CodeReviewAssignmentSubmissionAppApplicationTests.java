1311adc2563370f8ca7feb033c7ee34883a4a672
Adam Solomon <100500109+AdamSoloMe@users.noreply.github.com>
2023-08-22 13:44:41 -0400

    Revert "changed folder Strucutre"
    This reverts commit 41c7f4a270f6c909f599028d6214af711c5fa3c8.

--- src/test/java/com/guphub/CodeReviewAssignmentSubmissionApp/CodeReviewAssignmentSubmissionAppApplicationTests.java
+++ src/test/java/com/guphub/CodeReviewAssignmentSubmissionApp/CodeReviewAssignmentSubmissionAppApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class CodeReviewAssignmentSubmissionApplicationTests {
+class CodeReviewAssignmentSubmissionAppApplicationTests {
 
 	@Test
 	void contextLoads() {
