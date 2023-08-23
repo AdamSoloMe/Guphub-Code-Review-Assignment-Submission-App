bd2b570912fb026d890cba0ecd73b94af00e9db2
Adam Solomon <100500109+AdamSoloMe@users.noreply.github.com>
2023-08-23 00:26:37 -0400

    edits

--- Server/testing-cilent/guphub-testing/src/App.js
+++ Server/testing-cilent/guphub-testing/src/App.js
@@ -1,4 +1,3 @@
-
 import './App.css';
 import { useState } from 'react';
 import axios from 'axios';
@@ -8,7 +7,8 @@ function App() {
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
   const [accessToken, setAccessToken] = useLocalState('', 'access_token');
-  const [refreshToken, setRefreshToken] = useLocalState('', 'refresh_token');
+  const [refreshToken, setRefreshToken] = useLocalState('', 'refresh_token'); // Add this line
+  const [loginPerformed, setLoginPerformed] = useState(false); // Track login state
 
   const handleLogin = async (e) => {
     e.preventDefault();
@@ -20,21 +20,36 @@ function App() {
       });
 
       setAccessToken(response.data.access_token);
-      setRefreshToken(response.data.refresh_token);
+      setRefreshToken(response.data.refresh_token); // Set the refresh token
+      setLoginPerformed(true); // Mark login as performed
 
     } catch (error) {
       console.error('Login error:', error);
     }
   };
 
+  const handleRegister = async (e) => {
+    e.preventDefault();
+
+    try {
+      const response = await axios.post('/api/auth/register', {
+        username: username,
+        password: password,
+      });
+
+      console.log(response.data);
+    } catch (error) {
+      console.error('Registration error:', error);
+    }
+  };
+
   const handleRefresh = async () => {
     try {
       const response = await axios.post('/api/auth/refresh', {
-        refresh_token: refreshToken,
+        refresh_token: refreshToken, // Use the stored refresh token for refreshing
       });
 
-      setAccessToken(response.data);
-
+      setAccessToken(response.data); // New access token
     } catch (error) {
       console.error('Token refresh error:', error);
     }
@@ -58,13 +73,29 @@ function App() {
         />
         <button type="submit">Login</button>
       </form>
+      <h2>Register</h2>
+      <form onSubmit={handleRegister}>
+        <input
+          type="text"
+          placeholder="Username"
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
+        />
+        <input
+          type="password"
+          placeholder="Password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <button type="submit">Register</button>
+      </form>
       <button onClick={handleRefresh} disabled={!accessToken}>
         Refresh Token
       </button>
       <div>
-        Access Token: {accessToken}
-        <br />
-        Refresh Token: {refreshToken}
+          Access Token: {accessToken}
+          <br />
+          Refresh Token: {refreshToken}
       </div>
     </div>
   );
