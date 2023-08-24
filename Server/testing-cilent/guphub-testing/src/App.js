4b4b4a95a669c0368deb8177047d5cfc41c038e1
Adam Solomon <100500109+AdamSoloMe@users.noreply.github.com>
2023-08-24 15:36:10 -0400

    featuee: add website page naviagation

--- Server/testing-cilent/guphub-testing/src/App.js
+++ Server/testing-cilent/guphub-testing/src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route,Link } from 'react-router-dom';
 import Login from './Login/Login';
 import Register from './Register/Register';
 import Dashboard from './Dashboard';
@@ -8,17 +8,26 @@ import PrivateRoute from './PrivateRoute/PrivateRoute';
 
 function App() {
   return (
-
+    <div>
+      <nav>
+        <ul>
+          <li><Link to="/">Home</Link></li>
+          <li><Link to="/login">Login</Link></li>
+          <li><Link to="/register">Register</Link></li>
+          {/* Add more navigation links if needed */}
+        </ul>
+      </nav>
 
       <Routes>
         <Route path="/" element={<div>home</div>} />
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
         {/*<!-- Runs up to -->*/}
-        
-        <Route path="/Dashboard" element={<PrivateRoute component={<Dashboard />}/>}/>
+        <Route path="/Dashboard" element={<PrivateRoute component={<Dashboard />} />} />
       </Routes>
+    </div>
   );
 }
+
 
 export default App;
